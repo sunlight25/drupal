7f3ecfe48f6e933ca8f005c0b9cf3860e24efdc0
sunlight25 <suryagangwar281@gmail.com>
2017-09-10 13:17:20 +0530

    my first change

--- index.php
+++ index.php
@@ -13,9 +13,14 @@
 
 /**
  * Root directory of Drupal installation.
+ * 
  */
 define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
+
+echo "<pre>";
+echo "pre";
+
